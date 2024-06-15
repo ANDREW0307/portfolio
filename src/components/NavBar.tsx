a1e876500233b4cd8a0222920fcb23d7c3146c57
andrewli <andy.li3964@gmail.com>
2024-06-15 21:44:43 +0800

    compressed navbar background with rounded corners; video plays for all of landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,38 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import * as styles from './NavBar.module.css';
+import './NavBar.css';
 
 const NavBar: React.FC = () => {
+
+
   return (
-    <nav className="flex justify-end items-center p-4 bg-gray-800 text-white">
-      <Link to="/" className="px-4 py-1 font-bold no-underline hover:text-purple-600 transition duration-300">Home</Link>
-      <Link to="/about" className="px-4 py-1 font-bold no-underline hover:text-purple-600 transition duration-300">About Me</Link>
-      <Link to="/projects" className="px-4 py-1 font-bold no-underline hover:text-purple-600 transition duration-300">Projects</Link>
-      <a href="/static/images/resume.pdf" target="_blank" className="px-4 py-1 font-bold no-underline hover:text-purple-600 transition duration-300">Resume</a>
-      <a href="https://github.com/ANDREW0307" target="_blank" className="px-4 py-1 font-bold no-underline hover:text-purple-600 transition duration-300">
-        <img src="/static/images/github_logo_white_clear.svg" alt="GitHub" className="h-6 align-middle" />
-      </a>
+      <nav style={{
+        width: '100%',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        backgroundColor: 'rgba(0, 0, 0, 0)', // Transparent background for the entire navbar
+        display: 'flex',
+        justifyContent: 'flex-end', // Align links to the right
+        zIndex: 10, // Ensure navbar is above the video
+        padding: '0.5rem 1rem'
+      }}>
+     <div style={{
+        backgroundColor: '#424242', // Grey background only where the links are
+        borderRadius: '10px', // Optional: rounded corners for aesthetic
+        padding: '0.5rem 1rem'
+      }}>
+        <Link to="/" className="linkStyle">Home</Link>
+        <Link to="/about" className="linkStyle">About Me</Link>
+        <Link to="/projects" className="linkStyle">Projects</Link>
+        <a href="/static/images/resume.pdf" target="_blank" className="linkStyle">Resume</a>
+        <a href="https://github.com/ANDREW0307" target="_blank">
+          <img src="/static/images/github_logo_white_clear.svg" alt="GitHub" className="linkStyle"/>
+        </a>
+      </div>
     </nav>    
   );
 };
+
 
 export default NavBar;
